94a5768ed90418eeda87747b06c5bf1fe46d1e25
facundotopa <facundotopa738@gmail.com>
2022-11-30 19:31:03 -0300

    subida de archivos

--- server.js
+++ server.js
@@ -1,7 +1,26 @@
 const express = require ('express')
 const app=express();
+const multer= require('multer');
+const mimeType= require('mime-types');
+
+const storage = multer.diskStorage({
+   destination: 'uploads/',
+   filename: function (req,file,cb){
+       cb("",Date.now() + "." + mimeType.extension(file.mimetype))
+   }
+
+})
+const upload=multer({
+    storage:storage
+})
 
 app.get("/",(req,res)=>{
-    res.send("Hola Mundo");
+    console.log(__dirname)
+    res.sendFile(__dirname+"/html/tecnologia.html");
 })
+app.post("/files",upload.single('avatar'),(req,res)=>{
+    res.send("Todo Bien!");
+    }
+
+)
 app.listen(8080,()=> console.log("Server start"));
